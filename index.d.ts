10e8efbae4d8d854bf22eb21fb07dde2733885c0
Luke Edwards <luke.edwards05@gmail.com>
2021-05-29 21:26:07 -0700

    fix: types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,8 @@
-export type Compiler<T extends Record<string, any>> = (data: T) => string;
-export function transform(input: string, options?: any): string;
+export interface Options {
+	minify?: boolean;
+}
+
+export function transform(input: string, options?: Options): string;
+
+export type Compiler = <T extends Record<string, any>> (data: T) => string;
 export function compile(template: string): Compiler;
