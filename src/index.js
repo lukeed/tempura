8a423e07274a0cc5e5d65550c149a82d3d62f0c9
Luke Edwards <luke.edwards05@gmail.com>
2021-06-04 07:19:25 -0700

    chore: bump `bundt` version

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ export function transform(input, options) {
 	return (
 		options && options.format === 'cjs'
 		? 'var{esc:$$1}=require("tempura/utils");module.exports='
-		: 'import'+'{esc as $$1}from"tempura/utils";export'+' default '
+		: 'import{esc as $$1}from"tempura/utils";export default '
 	) + 'function($$2){' + utils.gen(input, options) + '}';
 }
