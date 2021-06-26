118a3c0f648a7ae440f694a53dc2a472b206656a
Luke Edwards <luke.edwards05@gmail.com>
2021-06-26 16:41:34 -0700

    chore: misc updates

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import * as utils from 'tempura/utils';
 
-export function compile(input, options) {
-	return new Function('$$1', '$$2', '$$3', utils.gen(input, options)).bind(0,
-		options && options.escape || utils.esc,
-		options && options.blocks
-	);
+export function compile(input, options={}) {
+	return new Function('$$1', '$$2', '$$3', utils.gen(input, options)).bind(0, options.escape || utils.esc, options.blocks);
 }
 
-export function transform(input, options) {
+export function transform(input, options={}) {
 	return (
-		options && options.format === 'cjs'
+		options.format === 'cjs'
 		? 'var{esc:$$1}=require("tempura/utils");module.exports='
 		: 'import{esc as $$1}from"tempura/utils";export default '
 	) + 'function($$3,$$2){'+utils.gen(input, options)+'}';
