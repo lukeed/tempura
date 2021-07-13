6c4228d621261b688750061b4168eae44736be4e
Luke Edwards <luke.edwards05@gmail.com>
2021-07-12 20:55:53 -0700

    chore: correct type definitions

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,14 +1,16 @@
 export type Args = Record<string, any>;
-export type Block<T extends Args = Args> = (args: T) => string;
-export type Compiler = <T extends Record<string, any>> (data: T) => string;
+export type Blocks = Record<string, Compiler>;
+export type Compiler = <T extends Args> (data?: T, blocks?: Blocks) => Promise<string>|string;
 
 export interface Options {
 	props?: string[];
-	blocks?: Record<string, Block>;
+	blocks?: Blocks;
 	async?: boolean;
 }
 
 export function esc<T=unknown>(value: T): T|string;
-
-export function compile(input: string, options?: Options & { escape?: typeof esc; }): Compiler;
 export function transform(input: string, options?: Options & { format?: 'esm' | 'cjs' }): string;
+
+export function compile(input: string, options?: Options & { async: true }): <T extends Args> (data: T) => Promise<string>;
+export function compile(input: string, options?: Options & { async: false }): <T extends Args> (data: T) => string;
+export function compile(input: string, options?: Options & { escape?: typeof esc; }): Compiler;
