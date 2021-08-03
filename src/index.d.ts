87de678ebefc28623c4187109bdbf6b9c553e7c9
Luke Edwards <luke.edwards05@gmail.com>
2021-08-02 18:22:26 -0700

    fix: update types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,9 @@
 export type Args = Record<string, any>;
 export type Blocks = Record<string, Compiler>;
-export type Compiler = <T extends Args> (data?: T, blocks?: Blocks) => Promise<string>|string;
+
+export type Compiler<T extends Args = Args> =
+	| ((data: T, blocks?: Blocks) => Promise<string>|string)
+	| ((data?: T) => Promise<string>|string)
 
 export interface Options {
 	loose?: boolean;
@@ -12,6 +15,7 @@ export interface Options {
 export function esc<T=unknown>(value: T): T|string;
 export function transform(input: string, options?: Options & { format?: 'esm' | 'cjs' }): string;
 
-export function compile(input: string, options?: Options & { async: true }): <T extends Args> (data: T) => Promise<string>;
-export function compile(input: string, options?: Options & { async: false }): <T extends Args> (data: T) => string;
-export function compile(input: string, options?: Options & { escape?: typeof esc; }): Compiler;
+type CompileOptions = Options & { escape?: typeof esc };
+export function compile(input: string, options?: CompileOptions & { async: true }): <T extends Args> (data?: T, blocks?: Blocks) => Promise<string>;
+export function compile(input: string, options?: CompileOptions & { async?: false }): <T extends Args> (data?: T, blocks?: Blocks) => string;
+export function compile(input: string, options?: CompileOptions): Compiler;
